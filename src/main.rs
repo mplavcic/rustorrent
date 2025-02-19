7c44b57eec07956347df5d19512dc7963a65b131
Mateo Plavcic <mateo.plava@gmail.com>
2025-02-19 14:21:49 +0100

    implement decoding bencoded values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod bencode;
+
+use bencode::decode_bencoded_value;
+use clap::Parser;
+use clap::Subcommand;
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Decode { encoded_value: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Decode { encoded_value } => {
+            let decoded_value = decode_bencoded_value(&encoded_value);
+            println!("{}", decoded_value.to_string());
+        }
+    }
 }
